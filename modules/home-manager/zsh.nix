db31c156fc908be95a0f01c456ae4a9c57c0ff25
Jacopown <jacopown@NixOS>
2024-12-02 11:29:08 +0100

    added some settings

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -4,12 +4,18 @@
     zsh = {
       enable = true;
       autosuggestion.enable = true;
+      dotDir = ".config/zsh";
+      history.append = true;
+      history.expireDuplicatesFirst = true;
+      history.ignoreAllDups = true;
       historySubstringSearch = { 
         enable = true;
-	searchDownKey = "$terminfo[kcud1]";
-	searchUpKey = "^$terminfo[kcuu1]";
+        searchDownKey = "$terminfo[kcud1]";
+        searchUpKey = "^$terminfo[kcuu1]";
       };
       syntaxHighlighting.enable = true;
+      sessionVariables = {};
+      shellAliases = {};
     };
     thefuck = {
       enable = true;
